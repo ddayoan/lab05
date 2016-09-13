6bdc3ecb9e4a9201c6af1484e46fd71f9a9336ed
DELANEY DAYOAN <delaneydayoan@adelphi.internal>
2016-09-13 12:06:12 -0400

    finish later

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -1,11 +1,72 @@
 
 /**
- * Write a description of class PersonCreator here.
+ * I am creating a person.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Delaney Dayoan) 
+ * @version (September 13, 2016)
  */
 public class PersonCreator
 {
-    // Your class begins here!
+    private String name; 
+    private int age;
+    private String sex;
+    private int height;
+    private int weight;
+    private String socialSecurityNumber;
+    private String address;
+    private String phoneNumber;
+    private String dob;
+    
+    public PersonCreator(String first, String last)
+    {
+        name= first + " " + last;
+        age= 0;
+        sex= " ";
+        height= 0;
+        weight= 0;
+        socialSecurityNumber= " ";
+        address= " ";
+        phoneNumber= " ";
+        dob= " ";
+    }
+    
+    public void getAge(int years)
+    {
+        age= years;
+    }
+    
+    public void getSex(String userSex)
+    {
+        sex= userSex;
+    }
+    
+    public void getHeight(int inches)
+    {
+        height= inches;
+    }
+    
+    public void getWeight(int pounds)
+    {
+        weight= pounds;
+    }
+    
+    public void getSocialSecurityNumber(String userSocialSecurityNumber)
+    {
+        socialSecurityNumber= userSocialSecurityNumber;
+    }
+    
+    public void getAddress(String housenumber, String streetname, String city, String state, String zipcode)
+    {
+        address= housenumber + streetname + city + state + zipcode;
+    }
+    
+    public void getPhoneNumber(String userPhonenumber)
+    {
+        phoneNumber= userPhonenumber;
+    }
+    
+    public void getDob(String userDob)
+    {
+        dob= userDob;
+    }
 }
